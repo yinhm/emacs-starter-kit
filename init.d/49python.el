9d19345e09db5ba8164ac56ec4bdebfa5ade6ead
HaiMing.Yin <HaiMing.Yin@gmail.com>
2011-01-06 15:55:36 +0800

    minor update

--- init.d/49python.el
+++ init.d/49python.el
@@ -57,6 +57,19 @@
 ;;(setq ropemacs-guess-project t)
 (rope-open-project "~/.pymacs")
 
+;; copy from https://github.com/gabrielelanaro/emacs-for-python/blob/master/epy-python.el
+;; ;; Adding hook to automatically open a rope project if there is one
+;; ;; in the current or in the upper level directory
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;             (cond ((file-exists-p ".ropeproject")
+;;                    (rope-open-project default-directory))
+;;                   ((file-exists-p "../.ropeproject")
+;;                    (rope-open-project (concat default-directory "..")))
+;;                   )))
+;; )
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Auto-completion
 ;;; Integrates:

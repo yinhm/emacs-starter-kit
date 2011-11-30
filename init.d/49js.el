dcc49d847eff80c10f54d55c29b5bf009313c3a0
HaiMing.Yin <HaiMing.Yin@gmail.com>
2011-11-30 13:18:21 +0800

    Change to jslint-emacs

--- init.d/49js.el
+++ init.d/49js.el
@@ -15,7 +15,7 @@
          (local-file (file-relative-name
 		      temp-file
 		      (file-name-directory buffer-file-name))))
-    (list "jslint" (list local-file))))
+    (list "jslint-emacs" (list local-file))))
 
 (defun flymake-jslint-enable ()
   (push '(".+\\.js$" flymake-jslint-init) flymake-allowed-file-name-masks)

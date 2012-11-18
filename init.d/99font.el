6342290c8eec0b711ce90cb4e1b41d7fd65659b3
haigent <HaiMing.Yin@gmail.com>
2012-11-19 00:24:23 +0800

    Fixed a typo.

--- init.d/99font.el
+++ init.d/99font.el
@@ -4,7 +4,7 @@
 ;; specify a unicode font : MENLO (forced normal)
 ;;(set-fontset-font "fontset-default" 'unicode "-apple-Menlo-medium-normal-normal-*-13-*-*-*-m-0-iso10646-1")
 
-(set-frame-font "Inconsolata-14`")
+(set-frame-font "Inconsolata-14")
 (set-fontset-font
     (frame-parameter nil 'font)
     'han
